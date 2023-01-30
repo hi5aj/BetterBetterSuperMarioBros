6fc62fd8f12837cb6b52d2c7a614b7407890bba4
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2023-01-30 14:02:58 -0500

    added controllers
    added player controller and camera controller

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour{
+    public GameObject followObject;
+    public Vector2 followOffset;
+    public float speed = 3f;
+    private Vector2 threshold;
+    private Rigidbody2D rb;
+    
+        [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
+    private Vector3 velocity;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        threshold = calculateThreshold();
+        rb = followObject.GetComponent<Rigidbody2D>();
+    }
+
+    void update()
+    {
+
+       
+        Vector2 follow = followObject.transform.position;
+        float xDifference = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+        float yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+
+        Vector3 newPosition = transform.position;
+        if(Mathf.Abs(xDifference) >= threshold.x){
+            newPosition.x = follow.x;
+        }
+        if(Mathf.Abs(yDifference) >= threshold.y){
+            newPosition.y = follow.y;
+        }
+        float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+                Vector3 startPos = transform.position;
+
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+                transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
+        
+    }
+    private Vector3 calculateThreshold(){
+        Rect aspect = Camera.main.pixelRect;
+        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+        t.x -= followOffset.x;
+        t.y -= followOffset.y;
+        return t;
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Vector2 border = calculateThreshold();
+        Gizmos.DrawWireCube(transform.position, new Vector3(border.x * 2, border.y * 2, 1));
+
+                Gizmos.color = Color.red;
+        //top boundary line
+        Gizmos.DrawLine(new Vector2(leftLimit,topLimit), new Vector2(rightLimit, topLimit));
+        //right boundary line
+        Gizmos.DrawLine(new Vector2(rightLimit,topLimit), new Vector2(rightLimit, bottomLimit));
+        //bottom boundary line
+        Gizmos.DrawLine(new Vector2(rightLimit,bottomLimit), new Vector2(leftLimit, bottomLimit));
+        //left boundary line
+        Gizmos.DrawLine(new Vector2(leftLimit,bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+}
